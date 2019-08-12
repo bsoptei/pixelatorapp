c5f5a957f8e0ef7c2d9da4baef4b72e6f3ac1a47
bsoptei <bsoptei@gmail.com>
2019-08-12 21:42:32 +0200

    add static 40x htmls, update server code a bit

--- server.js
+++ server.js
@@ -4,39 +4,36 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 http.createServer((request, response) => {
-    var filePath = '.' + request.url;
-    if (filePath == './') {
-        filePath = './index.html';
-    }
+  const url = request.url;
+  const urlUpdated = `.${url[1] == '?' ? '/' : url}`;
+  const filePath = urlUpdated == './' ? './index.html' : urlUpdated;
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const writeResponse = code => contentType => content => {
+    response.writeHead(code, { 'Content-Type': contentType });
+    response.end(content, 'utf-8');
+  };
 
-    const extname = String(path.extname(filePath)).toLowerCase();
-    const mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.ico': 'image/x-icon'
-    };
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.ico': 'image/x-icon',
+    '': 'text/html'
+  };
 
-    const contentType = mimeTypes[extname] || 'application/octet-stream';
+  const contentType = mimeTypes[extname];
 
+  if (contentType == undefined) {
+    fs.readFile('./400.html', (_, content) => writeResponse(400)('text/html')(content));
+  } else {
     fs.readFile(filePath, (error, content) => {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', (error, content) => {
-                    response.writeHead(404, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
+      if (error) {
+        fs.readFile('./404.html', (_, content) => writeResponse(404)(contentType)(content));
+      } else {
+        writeResponse(200)(contentType)(content);
+      }
     });
+  }
 
 }).listen(PORT);
 console.log(`Server running at http://127.0.0.1:${PORT}/`);
